3120923866c971625f430475a53cb7001ab78ef9
Motti Gottlieb <mottigottlieb@gmail.com>
2019-11-12 19:41:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+// describe("index.js", () => {
+//   describe("greet() function", () => {
+//     it("Returns Good Morning before 12pm", () => {
+//       expect(greet("8:40")).to.equal('Good Morning')
+//     })
+
+
+function greet() {
+  
+}
+
+
+
+
+
+
+
+
 /* Write your implementation of displayMessage() */
+
+
+
